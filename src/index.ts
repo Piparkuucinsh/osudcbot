66fe25f6fc466e825a893d071f2c5f21c864a981
Piparkuucinsh <audaja107@gmail.com>
2023-10-31 12:04:24 +0200

    setup

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import "dotenv/config";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
+  ],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.BOT_TOKEN);
