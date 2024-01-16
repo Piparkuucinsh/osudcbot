229b20a598ed4f6a691bd8bf870a5d2a951646d3
Piparkuucinsh <audaja107@gmail.com>
2024-01-16 15:23:15 +0200

    add await

--- src/utils/sendBotMessage.ts
+++ src/utils/sendBotMessage.ts
@@ -2,11 +2,9 @@ import { config } from "config";
 import { ChannelType, MessageCreateOptions, MessagePayload } from "discord.js";
 import { client } from "index";
 
-
-
 export const sendBotMessage = async (msg: string | MessagePayload | MessageCreateOptions) => {
     const ctx = await client.channels.fetch(config.bot_channel_id);
     if (ctx && ctx.type === ChannelType.GuildText) {
-        ctx.send(msg)
+        await ctx.send(msg)
     }
 }
