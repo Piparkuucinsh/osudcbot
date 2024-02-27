9d2fcab13e571a509742f88e6d7dd792ed1c0f97
Adriāns Piliksers <103249663+ASomniphobeHere@users.noreply.github.com>
2024-02-27 12:12:25 +0200

    Add refreshToken(for reference)

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,25 @@
 import { Client, Events } from "discord.js";
 import { EventModule } from "types";
+import { refreshToken } from "../utils/refreshToken";
 
 const ReadyEventModule: EventModule<Events.ClientReady> = {
   name: Events.ClientReady,
   once: false,
   execute: (c: Client<true>) => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log("Connected to the following guilds:");
+    c.guilds.cache.forEach(guild => {
+      console.log(`- ${guild.name} (ID: ${guild.id})`);
+    });
+    const tokenResetInterval = 10 * 1000;// 12 * 60 * 60 * 1000; // 12 hours in milliseconds
+    // setInterval(async () => {
+    //   try {
+    //     await refreshToken();
+    //     console.log('Token refreshed successfully');
+    //   } catch (error) {
+    //     console.error('Error refreshing token:', error);
+    //   }
+    // }, tokenResetInterval);
   },
 };
 
