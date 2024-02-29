a2a05169c89e73dfd5f1ef12572b6252bb8caa37
Piparkuucinsh <audaja107@gmail.com>
2024-02-29 16:09:04 +0200

    add member join and leave

--- src/events/onMemberJoin.ts
+++ src/events/onMemberJoin.ts
@@ -1,23 +1,25 @@
-// import { Events, GuildMember, Presence } from "discord.js";
-// import { config } from "../config";
-// import { EventModule } from "types";
+import { Events, GuildMember } from "discord.js";
+import { EventModule } from "@/types";
+import { prisma } from "@/lib/prisma";
 
-// const presenceUpdateEvent: EventModule<Events.GuildMemberAdd> = {
-//   name: Events.GuildMemberAdd,
-//   once: false,
-//   execute: (member: GuildMember) => {
-//     try {
-//       // console.log(newPresence);
-//       if (!newPresence.guild) {
-//         return;
-//       }
-//       if (newPresence.guild.id != config.server_id) {
-//         return;
-//       }
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   },
-// };
+const presenceUpdateEvent: EventModule<Events.GuildMemberAdd> = {
+  name: Events.GuildMemberAdd,
+  once: false,
+  execute: async (member: GuildMember) => {
+    try {
+      const user = await prisma.user.findFirst({ where: { discord_user_id: BigInt(member.id) } })
+      if (user) {
+        //send welcome message welcoming back
+        await prisma.user.update({ where: { discord_user_id: BigInt(member.id) }, data: { in_server: true } })
+      } else {
+        //send welcome message
+        await prisma.user.create({ data: { discord_user_id: BigInt(member.id), in_server: true } })
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
 
-// export default presenceUpdateEvent;
+export default presenceUpdateEvent;
+
