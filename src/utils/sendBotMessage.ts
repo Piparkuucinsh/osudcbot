2e6c916d7aa21edb1784022e9d420ff9599bd7f0
Piparkuucinsh <audaja107@gmail.com>
2024-04-02 10:42:02 +0300

    fix import

--- src/utils/sendBotMessage.ts
+++ src/utils/sendBotMessage.ts
@@ -1,4 +1,4 @@
-import { config } from '@/../config'
+import { config } from '@/config'
 import { ChannelType, MessageCreateOptions, MessagePayload } from 'discord.js'
 import { client } from '@/index'
 
