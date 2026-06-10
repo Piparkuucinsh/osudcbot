14747fabfa6f5478effb33b8f59f22eff53b2fde
Piparkuucinsh <audaja107@gmail.com>
2026-06-10 15:13:11 +0200

    refactor: remove redundant logging from presence update event handler

--- src/events/presenceUpdate.ts
+++ src/events/presenceUpdate.ts
@@ -1,7 +1,7 @@
 import { Events, type Presence } from "discord.js";
 import { linkAccounts } from "@/features/linkAccounts";
 import { config } from "@/init/config";
-import { error, info } from "@/lib/log";
+import { error } from "@/lib/log";
 import type { EventModule } from "@/types";
 
 const presenceUpdateEvent: EventModule<Events.PresenceUpdate> = {
@@ -9,16 +9,12 @@ const presenceUpdateEvent: EventModule<Events.PresenceUpdate> = {
 	once: false,
 	execute: (_oldPresence: Presence | null, newPresence: Presence) => {
 		try {
-			// console.log(newPresence);
 			if (!newPresence.guild) {
 				return;
 			}
-			info(`Presence update guild id: ${newPresence.guild.id}`);
-			info(`Config server id: ${config.discord.serverId}`);
 			if (newPresence.guild.id !== config.discord.serverId) {
 				return;
 			}
-			info("Linking accounts...");
 			void linkAccounts(newPresence);
 		} catch (err) {
 			error(String(err));
